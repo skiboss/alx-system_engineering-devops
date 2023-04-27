61ac875a18553304c9c1a59599501705974670a7
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2023-04-28 00:52:55 +0100

    Create: Manifest to create a file with the given configurations

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file with the desired configuration using puppet in /tmp
+
+file {'/tmp/school':
+  mode    => '0744',
+  owner   => 'www.data',
+  group   => 'www.data',
+  content => 'I love Puppet'
+}
+
