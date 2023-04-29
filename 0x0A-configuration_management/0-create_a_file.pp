aa3bf56812b4509f730704c32e983c4d8c94584a
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2023-04-29 01:03:57 +0100

    Fix: hostname(www-data) in file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file with the desired configuration using puppet in /tmp
 
 file {'/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
-  owner   => 'www.data',
-  group   => 'www.data',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
 
